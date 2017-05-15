8a142ec98cf8d68951a621f1063be3b6eba73044
zakAsur26 <baptiste.fotia.lpasur16-17@gv5.fr>
2017-05-16 00:17:17 +0200

    Ajout d'un new group replicat dfs, ajout des membres, selec server primaire

--- install-dfsr.ps1
+++ install-dfsr.ps1
@@ -3,3 +3,20 @@ Install-WindowsFeature -Name FS-DFS-Replication -IncludeManagementTools -Include
 
 # Installation de l'outil de gestion DFS
 Install-WindowsFeature -Name RSAT-DFS-Mgmt-Con -IncludeManagementTools -IncludeAllSubFeature
+
+# Creation d'un nouveau groupe de replication DFS
+New-DfsReplicationGroup -GroupeName "NomDuGroupe" -Description "Replication DFSR" -DomainName "NameDomaine.com" -WhatIf
+
+# Ajout des membres
+
+Add-DfsrMember -GroupeName "NomDuGroupe" -ComputerName "Srv1","Srv2"
+
+# Choix du mode topologique
+
+# Planification du groupe de replication et bande passante
+
+# Selection du serveur principal qui contient les donnees initiales
+  # -FolderName : Liste de dossier a Repliquer
+  # -ContentPath : Chemin local de destination des fichiers qui seront repliques
+  # -ComputerName : Nom SRV qui sera membre primaire
+Set-DfsrMembership -GroupName "NomDuGroupe" -FolderName "Liste/Dossier/A/Repliquer" -ContentPath "Chemin/Dossier/local/pour/contenir/ficchiers/replique" -ComputerName "NomServeurPrimaire" -PrimaryMember $True -Force | Format-Table *name,*path,primary* -auto -wrap
