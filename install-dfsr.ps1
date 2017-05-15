e96943cd2628b105f494c67d69300e62e72e8d1c
zakAsur26 <baptiste.fotia.lpasur16-17@gv5.fr>
2017-05-15 17:13:39 +0200

    Ajout des outils de gestion DFS pour DFSR

--- install-dfsr.ps1
+++ install-dfsr.ps1
@@ -1,2 +1,5 @@
 # Installation du service DFSR
 Install-WindowsFeature -Name FS-DFS-Replication -IncludeManagementTools -IncludeAllSubFeature
+
+# Installation de l'outil de gestion DFS
+Install-WindowsFeature -Name RSAT-DFS-Mgmt-Con -IncludeManagementTools -IncludeAllSubFeature
